4ab5a99f50a27d8c85a1ce72713f08b2504e6c71
= <anton.graham.2011@gmail.com>
2022-10-21 21:33:37 -0400

    Create Eloquent Relation to category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+namespace App\Category;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    //Eloquent Relation ???
+    public function category()
+    {
+        $this->belongsTo(Category::class);
+    }
 }
