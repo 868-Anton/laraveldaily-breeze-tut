7bf5693fc2a2186a66e3ea7022c9d7dbc571ebcc
= <anton.graham.2011@gmail.com>
2022-10-22 00:42:02 -0400

    Added request class and create validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,6 +9,15 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg border-black border-2">
               <div class="p-6 bg-white border-b border-gray-200 ">
+                @if ($errors->any())
+                  <div class="text-red-500">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                  </div>
+                @endif
                 <form action="{{ route('posts.store') }}" method="post">
                   @csrf
                   Title: 
